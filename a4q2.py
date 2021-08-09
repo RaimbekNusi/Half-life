059fe98997ef29d720b48bf88cfe6e3e2e536c74
RaimbekNusi <86201781+RaimbekNusi@users.noreply.github.com>
2021-08-09 23:13:17 +0600

    Add files via upload

---
+++ a4q2.py
@@ -0,0 +1,15 @@
+"Raimbek Nussipkhozhin, NFU892, 11313819, Jeffrey Long"
+import math as math
+m = int(input("Enter the initial mass of the material: "))
+while m <= 0:
+    print("Error! The mass of the material must be a positive number!")
+    m = int(input("Enter the initial mass of the material: "))
+h = int(input("Enter half-life in days: "))
+while h <= 0:
+    print("Error! Half-life must be a positive number!")
+    h = int(input("Enter half-life in days: "))
+t = -1
+while t < 20:
+    t = t + 1
+    mt = m * 0.5 ** (t / h)
+    print("After day " + str(t) + " there are " + str(mt) + "grams remaining.")
